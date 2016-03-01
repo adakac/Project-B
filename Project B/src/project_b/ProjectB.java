bc00bba15956b4036f036fbc9fbead6b5ab037ff
A3977 <>
2016-03-01 10:32:15 +0100

    Testcommit und Init

---
+++ Project B/src/project_b/ProjectB.java
@@ -0,0 +1,19 @@
+
+package project_b;
+
+import javax.swing.JOptionPane;
+
+public class ProjectB
+{
+    private void testmethod()
+    {
+        JOptionPane.showMessageDialog(null, "Test Example");
+    }
+    
+    public static void main(String[] args)
+    {
+        ProjectB pb = new ProjectB();
+        pb.testmethod();
+    }
+    
+}
